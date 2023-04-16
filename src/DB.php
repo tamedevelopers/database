267321fcd09ba9dd4cd91ea3487e36e400444566
Fredrick Peter <tamdevelopers@gmail.com>
2023-04-17 00:16:51 +0100

    Development

--- src/DB.php
+++ src/DB.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * This file is part of ultimate-orm-database.
  *
  * (c) Tame Developers Inc.
- *
+ * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
