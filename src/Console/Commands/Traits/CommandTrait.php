4e2ac21d24bff4f9d6bc3fdc3fa4f725126db44f
Fredrick Peter <tamedevelopers@gmail.com>
2025-09-04 13:20:32 +0100

    update

--- src/Console/Commands/Traits/CommandTrait.php
+++ src/Console/Commands/Traits/CommandTrait.php
@@ -12,25 +12,6 @@ namespace Tamedevelopers\Database\Console\Commands\Traits;
 trait CommandTrait{
 
 
-
-
-    /**
-     * Get an option value from a list of arguments.
-     *
-     * @param array $args List of arguments passed to the script
-     * @param string $key Option key name (--option-name)
-     * @return string|null The option value or null if not found
-     */
-    protected function getOption(array $args, string $key): ?string
-    {
-        foreach ($args as $arg) {
-            if (str_starts_with($arg, "--{$key}=")) {
-                return substr($arg, strlen($key) + 3);
-            }
-        }
-        return null;
-    }
-
     /**
      * Split command into base and optional subcommand
      * @return array{0:string,1:?string}
