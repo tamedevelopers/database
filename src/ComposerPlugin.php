5b5e1eff3fc163a301b3d8d78d702918d6ef61a1
Fredrick Peter <tamedevelopers@gmail.com>
2025-09-04 07:09:58 +0100

    update

---
+++ src/ComposerPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tamedevelopers\Database;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Script\ScriptEvents;
+
+final class ComposerPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io): void {}
+    public function deactivate(Composer $composer, IOInterface $io): void {}
+    public function uninstall(Composer $composer, IOInterface $io): void {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'onPostInstall',
+            ScriptEvents::POST_UPDATE_CMD  => 'onPostUpdate',
+        ];
+    }
+
+    public function onPostInstall(): void
+    {
+        // Run package post-install logic
+        Installer::postInstall();
+    }
+
+    public function onPostUpdate(): void
+    {
+        // Run package post-update logic
+        Installer::postUpdate();
+    }
+}
